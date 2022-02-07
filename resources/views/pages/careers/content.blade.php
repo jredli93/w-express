1b87d1230bc56be509102f92726a68261e630225
Jovan Redli <>
2022-02-07 16:32:07 +0100

    application form

--- resources/views/pages/careers/content.blade.php
+++ resources/views/pages/careers/content.blade.php
@@ -56,10 +56,134 @@
                     Apply now online clicking below or call our office 972-787-0047 during Monday- Friday 7am to 5 pm
                     CST.
                 </p>
-                <a data-aos="fade-up" href="{{ route('about-us') }}"
+                <a data-aos="fade-up" href="#application-form" data-lity
                     class="btn careers-cta-left-btn btn--gradient btn--xlrg">
                     <span class="btn__text"><i class="fa fa-2x fa-file-pdf cta-pdf"></i>Fill in the application</span>
                 </a>
+                <div id="application-form" style="background:#fff" class="modalbox about-work-modal lity-hide">
+                    <div class="bg-effect"></div>
+                    <h1 class="modal-title">Initial Application</h1>
+                    <div class="modal-center">
+                        <div class="contact-form">
+                            <form action="#" class="input-form">
+
+                                <label>Full Name:</label>
+                                <input placeholder="First Middle Last" type="text" name="full-name" class="form-name">
+
+                                <label>Current Address: </label>
+                                <input placeholder="Street City State,Zip" type="text" class="form-name">
+
+                                <label>Previous Address(es): </label>
+                                <input placeholder="Street City State,Zip" type="text" class="form-name">
+
+                                <label>Previous Address(es): </label>
+                                <input placeholder="Street City State,Zip" type="text" class="form-name">
+
+                                <div class="contact-details">
+                                    <div class="contact-input-wrap">
+                                        <label>Phone: </label>
+                                        <input type="number" placeholder="(555) 555-1234" class="form-name">
+                                    </div>
+                                    <div class="contact-input-wrap">
+                                        <label>Date of Birth</label>
+                                        <input type="date" id="start" class="form-name" name="trip-start"
+                                            value="2018-07-22" min="2018-01-01" max="2018-12-31">
+                                    </div>
+                                    <div class="contact-input-wrap">
+                                        <label>Social Security: </label>
+                                        <input type="text" placeholder="xxx-xx-xxxx" class="form-name">
+                                    </div>
+                                </div>
+
+                                <div class="contact-details">
+                                    <div class="contact-input-wrap">
+                                        <label>Emergency Contact Name </label>
+                                        <input type="text" class="form-name">
+                                    </div>
+
+                                    <div class="contact-input-wrap">
+                                        <label>Relation</label>
+                                        <input type="text" class="form-name">
+                                    </div>
+                                </div>
+
+                                <div class="contact-details">
+                                    <div class="contact-input-wrap">
+                                        <label>Contact Address </label>
+                                        <input type="text" class="form-name">
+                                    </div>
+
+                                    <div class="contact-input-wrap">
+                                        <label>Phone #</label>
+                                        <input type="number" placeholder="(555) 555-1234" class="form-name">
+                                    </div>
+                                </div>
+
+                                <label class="contact-heading">Driver's license information: </label>
+                                <div class="contact-license">
+                                    <label>State: </label>
+                                    <input type="text" class="form-name">
+
+                                    <label>License #</label>
+                                    <input type="number" class="form-name">
+
+                                    <label>Type: </label>
+                                    <input type="text" class="form-name">
+
+                                    <label>Expiration Date: </label>
+                                    <input type="date" id="start" class="form-name" name="trip-start" value="2018-07-22"
+                                        min="2018-01-01" max="2018-12-31">
+                                </div>
+
+                                <label class="contact-heading">Driver experience </label>
+                                <div class="contact-license">
+                                    <label>Type of Equipment </label>
+                                    <input type="text" class="form-name">
+
+                                    <label>From</label>
+                                    <input type="date" id="start" class="form-name" name="trip-start" value="2018-07-22"
+                                        min="2018-01-01" max="2018-12-31">
+                                    <label>To </label>
+                                    <input type="date" id="start" class="form-name" name="trip-start" value="2018-07-22"
+                                        min="2018-01-01" max="2018-12-31">
+
+                                    <label>Miles</label>
+                                    <input placeholder="Approx. number" type="number" class="form-name">
+                                </div>
+
+                                <div class="contact-box-wrap">
+                                    <span class="contact-radio-text">Have you ever been denied a license, permit or
+                                        privilege to operate a motor
+                                        vehicle?</span>
+                                    <div class="contact-box-radio-wrap">
+                                        <input type="radio" name="contact-radio-denied" value="Yes"><span>Yes</span>
+                                        <input type="radio" name="contact-radio-denied" value="No"><span>No</span>
+                                    </div>
+                                </div>
+
+                                <div class="contact-box-wrap">
+                                    <span class="contact-radio-text">Has any license, permit or privilege ever been
+                                        suspended or revoked?</span>
+                                    <div class="contact-box-radio-wrap">
+                                        <input type="radio" name="contact-radio-revoked" value="Yes"><span>Yes</span>
+                                        <input type="radio" name="contact-radio-revoked" value="No"><span>No</span>
+                                    </div>
+                                </div>
+
+                                    <span class="contact-radio-text">I certify that I have authorized West Express LLC
+                                        to perform my Driving Records (MVR) check as a requirement of employment at the
+                                        company listed above.</span>
+                                    <input type="checkbox" name="contact-checkbox-accept" value="I accept">
+
+
+                                <a href="#" style="margin-top:0.3rem" class="btn btn--xlrg btn--gradient">
+                                    <span class="btn__text btn-text">Send</span>
+                                </a>
+
+                            </form>
+                        </div>
+                    </div>
+                </div>
             </div>
             <div class="careers-cta-right">
                 <div class="careers-cta-right-container">
